bb09bcc5208e750cc399470a76f78d6e80331f98
Timo- <Timo-@DESKTOP-DG5BRMU>
2018-02-14 18:42:45 +0100

    Test Command Added

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -2,6 +2,14 @@ package Main;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Main extends JavaPlugin{
+import commands.CMDtest;
 
+public class Main extends JavaPlugin{
+	public void onEnable() {
+		
+    	this.getCommand("test").setExecutor(new CMDtest());
+    	
+    	System.out.println("Das Plugin wurde aktiviert!");	
+
+}
 }

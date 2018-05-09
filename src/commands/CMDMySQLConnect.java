6634be01175eef226bd8e935d4a4afc0b905cc44
Timo- <Timo-@DESKTOP-DG5BRMU>
2018-05-09 23:59:21 +0200

    lelfuck2

---
+++ src/commands/CMDMySQLConnect.java
@@ -0,0 +1,26 @@
+package commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class CMDMySQLConnect implements CommandExecutor {
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+		Player p = (Player) sender;
+		if(command.getName().equals("connect"));
+		if(sender instanceof Player) {
+			if(!p.hasPermission("Noneless.MySQLConnect")) {
+			p.sendMessage("Nein böse");
+			return true;
+			
+		}else {
+			Main.MySQL.connect();
+			p.sendMessage("Du hast dich Verbunden");
+		}
+		}
+		return false;
+	}
+}
